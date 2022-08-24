96ba68914c44700ff525f373ab74bb85169ef29b
王中奇 <wangzhongqi95@gmail.com>
2022-08-24 18:36:17 +0800

    增加mysql链接池

---
+++ src/util/mysqlUtil.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+var pool = mysql.createPool({
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "database",
+  port: 3306,
+});
+var query = function (sql, callback) {
+  pool.getConnection(function (err, conn) {
+    if (err) {
+      callback(err, null, null);
+    } else {
+      conn.query(sql, function (qerr, vals, fields) {
+        //释放连接
+        conn.release();
+        //事件驱动回调
+        callback(qerr, vals, fields);
+      });
+    }
+  });
+};
+export { query };
